940ef36401f4753321fb097d542f437497e044a9
Ricardo Órfão <ricardorfao@gmail.com>
2023-12-04 21:11:25 +0000

    feature/GZ-67: add object view less on piggy vault

---
+++ Object Repository/elem_View less.rs
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>elem_View less</name>
+   <tag></tag>
+   <elementGuidId>179473b5-ccf3-435b-bd80-ef12658b89ea</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>button.color-blue.font-thin</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='app']/div/div/div/div[4]/div[2]/button</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>261a10ed-6539-448c-a391-ec9f25f9e3f0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>color-blue font-thin</value>
+      <webElementGuid>1b654bc8-b771-46c7-8be8-b975d91e0e8c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value> View less</value>
+      <webElementGuid>c00f6128-5ed7-46a9-9569-0f49f76da187</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;app&quot;)/div[@class=&quot;main-container__container&quot;]/div[@class=&quot;main-container__container-inner&quot;]/div[@class=&quot;container__direction-column-start&quot;]/div[@class=&quot;mt-30 dashboard__row-upper&quot;]/div[@class=&quot;dashboard__container-top-content content-align-right&quot;]/button[@class=&quot;color-blue font-thin&quot;]</value>
+      <webElementGuid>9d759be4-b806-496d-8c05-a02d49d51285</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='app']/div/div/div/div[4]/div[2]/button</value>
+      <webElementGuid>214bb6a8-6c04-4962-86dc-1ec98ae59ad1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Piggy Transactions'])[1]/following::button[1]</value>
+      <webElementGuid>3565e01f-8ab8-4be3-9543-9159d872bae6</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)=concat('Seems like you don', &quot;'&quot;, 't have any movements in your account')])[1]/preceding::button[1]</value>
+      <webElementGuid>1884fc8a-3793-4a0e-b286-688f9cf1fe54</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Capture object:'])[1]/preceding::button[1]</value>
+      <webElementGuid>9ad0c549-1100-47f7-ad46-6a89c3f11dcf</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='View less']/parent::*</value>
+      <webElementGuid>d05722ad-aecb-46ad-bd49-98f6c95ad6bb</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//button</value>
+      <webElementGuid>c00c13f1-b33e-428c-9cde-c99625a505ff</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//button[(text() = ' View less' or . = ' View less')]</value>
+      <webElementGuid>188e8024-a3cd-4b72-9c18-baec37a17245</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>

948bd2bf01448dd7b5be6a7ec733111c8b48bee4
Ricardo Órfão <ricardorfao@gmail.com>
2023-12-13 00:46:39 +0000

    feature/GZ-103: add object no notifications

---
+++ Object Repository/elem_Seems like you dont have any notifications.rs
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>elem_Seems like you dont have any notifications</name>
+   <tag></tag>
+   <elementGuidId>2bf96afc-197c-4079-96ec-251f90b4e949</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='app']/div/div/div[2]/div/h1</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>h1.t-align-center.primary-font.mt-30</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h1</value>
+      <webElementGuid>2a6a0986-5c3b-4901-b823-b206d1131d21</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>t-align-center primary-font mt-30</value>
+      <webElementGuid>28d27b11-9047-4d37-90a1-9b40e72fbc65</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Seems like you don't have any notifications</value>
+      <webElementGuid>2c9e94f5-4a53-4a6b-a463-ccf42f208979</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;app&quot;)/div[@class=&quot;main-container__container&quot;]/div[@class=&quot;main-container__container-inner&quot;]/div[@class=&quot;space&quot;]/div[1]/h1[@class=&quot;t-align-center primary-font mt-30&quot;]</value>
+      <webElementGuid>ff4d1a91-1661-4c64-90b7-f4e0568b8171</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='app']/div/div/div[2]/div/h1</value>
+      <webElementGuid>94eb9206-b5e4-47d9-be2e-0b40569a0982</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Notifications'])[1]/following::h1[1]</value>
+      <webElementGuid>576c6ece-7a7b-4814-a7b5-02e4d924b234</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Home'])[1]/preceding::h1[1]</value>
+      <webElementGuid>868544aa-431e-4911-a3c4-d0eabdf18cee</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Transacations'])[1]/preceding::h1[1]</value>
+      <webElementGuid>1ccdb732-cf21-4b75-89ce-81e1ac3ab9d2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/h1</value>
+      <webElementGuid>ad72f508-8161-4c59-a7b9-0e752c5f6a73</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h1[(text() = concat(&quot;Seems like you don&quot; , &quot;'&quot; , &quot;t have any notifications&quot;) or . = concat(&quot;Seems like you don&quot; , &quot;'&quot; , &quot;t have any notifications&quot;))]</value>
+      <webElementGuid>e2f129a8-9a5a-461e-a485-90d93d6cf1a7</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
